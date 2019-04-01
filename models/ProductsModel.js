287ddc697e5c0fe15f9d3357dffaa9b94a3a715e
소윤호 <soyunho@MacBookPro.local>
2019-04-01 22:49:55 +0900

    삭제하기 구현ex_3
    삭제하기 구현ex_3

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var {autoIncrement} = require('mongoose-plugin-autoinc');
+var moment = require('moment');
 
 //생성될 필드명을 정한다.
 var ProductsSchema = new Schema({
@@ -13,8 +14,25 @@ var ProductsSchema = new Schema({
     }
 });
 
+//virtual 변수는 호출되면 실행하는 함수
+// Object create 의 get과 set과 비슷함
+//set은 변수의 값을 바꾸거나 셋팅하면 호출
+// get은 getDate변수를 호출하는 순간 날짜 월일이 찍힌다.
+ProductsSchema.virtual('getDate').get(function(){
+    // var date = new Date(this.created_at);
+    // return {
+    //     year : date.getFullYear(),
+    //     month : date.getMonth()+1, 
+    //     day : date.getDate()
+    // };
+    return moment(this.created_at).format('YYYY / MM / DD');
+});
+
 // 1씩 증가하는 primary key를 만든다.
 // model : 생성할 document 이름
 // field : primary key , startAt : 1부터 시작
 ProductsSchema.plugin( autoIncrement , { model : 'products' , field : 'id' , startAt : 1 });
+// products 라고하는 Collection을 내보내겠다는 의미
 module.exports = mongoose.model('products', ProductsSchema);
+
+
